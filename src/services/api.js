3debe917151c6551ca84790974738764c943788c
Josphat <musyokajosphat10@gmail.com>
2025-08-05 14:03:44 +0300

    few changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,80 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://farme-backend.onrender.com/api/',
 });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('access');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
+let isRefreshing = false;
+let refreshSubscribers = [];
 
+const subscribeTokenRefresh = (cb) => {
+  refreshSubscribers.push(cb);
+};
 
+const onRefreshed = (newAccessToken) => {
+  refreshSubscribers.map(cb => cb(newAccessToken));
+  refreshSubscribers = [];
+};
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      localStorage.getItem('refresh')
+    ) {
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          subscribeTokenRefresh((token) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            resolve(api(originalRequest));
+          });
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+        const res = await axios.post('https://farme-backend.onrender.com/api/token/refresh/', {
+          refresh: localStorage.getItem('refresh')
+        });
+
+        const newAccess = res.data.access;
+        localStorage.setItem('access', newAccess);
+        api.defaults.headers.common['Authorization'] = `Bearer ${newAccess}`;
+
+        onRefreshed(newAccess);
+        return api(originalRequest);
+      } catch (refreshError) {
+        
+        localStorage.removeItem('access');
+        localStorage.removeItem('refresh');
+        window.location.href = '/login';  
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
+
 
 
 
